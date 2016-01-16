40efed9da04c638eecf3166574957d7f6a677786
fishedee <306766045@qq.com>
2016-01-16 13:27:21 +0800

    加强set-cookie的判断，禁止cookie被缓存

--- module/cache.go
+++ module/cache.go
@@ -1,93 +1,92 @@
 package module
 
 import (
-	"encoding/gob"
-	"net/http"
-	"time"
 	"bytes"
-	"sync"
-	"strings"
+	"encoding/gob"
 	"github.com/coocood/freecache"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
 )
 
-type Cache struct{
-	cache *freecache.Cache
-	mutex *sync.Mutex
+type Cache struct {
+	cache      *freecache.Cache
+	mutex      *sync.Mutex
 	expireTime time.Duration
-	data map[string]bool
+	data       map[string]bool
 }
 
-type CacheResponse struct{
-	Header map[string][]string
+type CacheResponse struct {
+	Header     map[string][]string
 	StatusCode int
-	Body []byte
+	Body       []byte
 }
 
-func NewCache(size int,expireTime time.Duration)(*Cache){
+func NewCache(size int, expireTime time.Duration) *Cache {
 	if size == 0 {
 		return &Cache{}
-	}else{
+	} else {
 		return &Cache{
-			cache:freecache.NewCache(size),
-			mutex:&sync.Mutex{},
-			data:map[string]bool{},
-			expireTime:expireTime,
+			cache:      freecache.NewCache(size),
+			mutex:      &sync.Mutex{},
+			data:       map[string]bool{},
+			expireTime: expireTime,
 		}
-	}	
+	}
 }
 
-func (this *Cache)AcquireLock(url string)(bool){
+func (this *Cache) AcquireLock(url string) bool {
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
-	result,ok := this.data[url]
-	if ok == false || result == false{
+	result, ok := this.data[url]
+	if ok == false || result == false {
 		this.data[url] = true
 		return true
-	}else{
+	} else {
 		return false
 	}
 }
 
-func (this *Cache)ReleaseLock(url string){
+func (this *Cache) ReleaseLock(url string) {
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
 	this.data[url] = false
 }
 
-func (this *Cache)GetInner(url string)(*CacheResponse){
-	data,err := this.cache.Get([]byte(url))
-	if err != nil{
+func (this *Cache) GetInner(url string) *CacheResponse {
+	data, err := this.cache.Get([]byte(url))
+	if err != nil {
 		return nil
 	}
 
 	var result CacheResponse
-    buf := bytes.NewBuffer(data)  
-    enc := gob.NewDecoder(buf)  
-    err = enc.Decode(&result)  
-    if err != nil {  
-    	Logger.Error("decode error!"+err.Error())
-        return nil
-    }
+	buf := bytes.NewBuffer(data)
+	enc := gob.NewDecoder(buf)
+	err = enc.Decode(&result)
+	if err != nil {
+		Logger.Error("decode error!" + err.Error())
+		return nil
+	}
 	return &result
 }
 
-
-func (this *Cache)SetInner(url string,response *CacheResponse,expireTime time.Duration){
+func (this *Cache) SetInner(url string, response *CacheResponse, expireTime time.Duration) {
 	buf := bytes.NewBuffer(nil)
-    enc := gob.NewEncoder(buf)  
-    err := enc.Encode(response)
-    if err != nil {
-    	Logger.Error("encode error!"+err.Error())
-        return
-    }
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(response)
+	if err != nil {
+		Logger.Error("encode error!" + err.Error())
+		return
+	}
 
-    this.cache.Set([]byte(url),buf.Bytes(),int(expireTime.Seconds()))
-    return
+	this.cache.Set([]byte(url), buf.Bytes(), int(expireTime.Seconds()))
+	return
 }
 
-func (this *Cache)GetCacheUrlString(request *http.Request)(string){
+func (this *Cache) GetCacheUrlString(request *http.Request) string {
 	requestUrl := *request.URL
 	requestQueryParam := requestUrl.Query()
 	requestQueryParam.Del("t")
@@ -97,47 +96,47 @@ func (this *Cache)GetCacheUrlString(request *http.Request)(string){
 	return requestUrlString
 }
 
-func (this *Cache)Get(request *http.Request)(*CacheResponse,bool){
-	if request.Method != "GET" || this.cache == nil{
-		return nil,false;
+func (this *Cache) Get(request *http.Request) (*CacheResponse, bool) {
+	if request.Method != "GET" || this.cache == nil {
+		return nil, false
 	}
 
 	cacheUrl := this.GetCacheUrlString(request)
 	response := this.GetInner(cacheUrl)
-	if response != nil{
-		return response,false
+	if response != nil {
+		return response, false
 	}
 
 	hasLock := this.AcquireLock(cacheUrl)
-	if hasLock == false{
-		response = this.GetInner(cacheUrl+"_old")
+	if hasLock == false {
+		response = this.GetInner(cacheUrl + "_old")
 	}
-	
-	return response,hasLock
+
+	return response, hasLock
 }
 
-func (this *Cache)Set(request *http.Request,response *CacheResponse,hasLock bool){
-	if request.Method != "GET" || this.cache == nil || hasLock == false{
-		return 
+func (this *Cache) Set(request *http.Request, response *CacheResponse, hasLock bool) {
+	if request.Method != "GET" || this.cache == nil || hasLock == false {
+		return
 	}
 	cacheUrl := this.GetCacheUrlString(request)
 	this.ReleaseLock(cacheUrl)
 
-	if response == nil{
+	if response == nil {
 		return
 	}
 	var newResponse CacheResponse
 	newResponse.StatusCode = response.StatusCode
 	newResponse.Body = response.Body
 	newResponse.Header = map[string][]string{}
-	for key,value := range response.Header{
+	for key, value := range response.Header {
 		lowerKey := strings.ToLower(key)
-		if lowerKey == "set-cookie"{
+		if strings.Index(lowerKey, "set-cookie") != -1 {
 			continue
 		}
 		newResponse.Header[key] = value
 	}
 
-	this.SetInner(cacheUrl,&newResponse,this.expireTime)
-	this.SetInner(cacheUrl+"_old",&newResponse,0)
+	this.SetInner(cacheUrl, &newResponse, this.expireTime)
+	this.SetInner(cacheUrl+"_old", &newResponse, 0)
 }
